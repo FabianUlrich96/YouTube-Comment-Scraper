fe353d85dda24d57141c84d9718ea885d0e6aaeb
Fabian Ulrich <fabian.ulrich@stud.uni-bamberg.de>
2022-03-02 14:56:21 +0100

    removed double iso format from tasks.py

--- backend/tasks.py
+++ backend/tasks.py
@@ -29,9 +29,7 @@ def query_videos(keys, job_id, query, published_before, published_after):
     date_range = pandas.date_range(after_object, before_object - timedelta(days=1), freq='d')
 
     for date_object in date_range:
-        after = date_object.isoformat("T") + "Z"
         before = date_object + timedelta(days=1)
-        before = before.isoformat("T") + "Z"
         execute_search_query(keys, job_id, query, before, after)
     job_db = session.query(Jobs).filter_by(job_id=job_id).first()
     job_db.done = True
